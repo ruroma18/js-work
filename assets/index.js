069ea8cfbfce684668661e4a06b5c35e70caa0bb
Roman Rudenko <ruroma18@gmail.com>
2022-02-22 11:57:25 +0200

    Add encryption text task

--- assets/index.js
+++ assets/index.js
@@ -1,29 +1,51 @@
 'use strict';
 
-const gessNumber = (number) => {
-  let startNum = 0;
-  let endNum = number;
-  let middleNum = Math.round(endNum / 2);
+// const gessNumber = (number) => {
+//   let startNum = 0;
+//   let endNum = number;
+//   let middleNum = Math.round(endNum / 2);
 
 
 
-  while (true) {
-    let askNum = confirm(middleNum);
+//   while (true) {
+//     let askNum = confirm(middleNum);
 
-    if (askNum === true) {
-      alert(middleNum);
-      return middleNum;
-    }
-    let isBiggerNum = confirm(`Bigger than? ${middleNum}`)
+//     if (askNum === true) {
+//       alert(middleNum);
+//       return middleNum;
+//     }
+//     let isBiggerNum = confirm(`Bigger than? ${middleNum}`)
 
-    if (isBiggerNum === true) {
-      startNum = middleNum;
-      middleNum = Math.ceil((endNum + startNum) / 2);
+//     if (isBiggerNum === true) {
+//       startNum = middleNum;
+//       middleNum = Math.ceil((endNum + startNum) / 2);
+//     } else {
+//       endNum = middleNum;
+//       middleNum = Math.floor((endNum + startNum) / 2);
+//     }
+//   }
+
+
+// }
+
+const changeLetters = new Map();
+changeLetters
+  .set('h', 'r')
+  .set('e', 'b')
+  .set('l', 'a')
+  .set('o', 'y')
+  .set('t', 'v')
+  .set('s', 'w')
+
+const encryptStr = (str) => {
+  let result = []
+  for (let i = 0; i < str.length; i++) {
+    if (changeLetters.has(str[i])) {
+      result.push(changeLetters.get(str[i]));
     } else {
-      endNum = middleNum;
-      middleNum = Math.floor((endNum + startNum) / 2);
+      result.push(str[i]);
     }
-  }
-  
 
-}
+  }
+  return result.join('');
+} 
