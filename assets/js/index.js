3929d870ad7be3803c423692a3459461a61049bb
Roman Rudenko <ruroma18@gmail.com>
2022-04-08 20:02:35 +0300

    Submit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,9 @@
 'use strict';
 
-const div = document.querySelector('#root');
-const [redBtn, greenBtn, blueBtn] = document.querySelectorAll('button');
- 
-const changeBg = (e) => {
-  const {target: {dataset: {colorBtn}}} = e;
-    div.style.backgroundColor = colorBtn;
-}
+const form = document.getElementById('login-form');
 
-redBtn.addEventListener('click', changeBg);
-greenBtn.addEventListener('click', changeBg);
-blueBtn.addEventListener('click', changeBg);
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); //предотвращает стандартное поведение( в данном случае - обновление страницы)
+  e.stopPropagation(); //останавливает "всплытие"
+  console.log(e);
+})
