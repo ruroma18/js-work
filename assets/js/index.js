c692e552ea65c8ece344dc0cbc98d5edcc698c82
Roman Rudenko <ruroma18@gmail.com>
2022-04-11 18:44:21 +0300

    Add delete function to todo-list task

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,17 +3,34 @@
 const form = document.getElementById('todo-form');
 const todoList = document.getElementById('todo-list');
 const todoState = [];
-  
+
 const addTask = (e) => {
   e.preventDefault();
-  const {target, target: { elements } } = e;
-  const taskText = elements.task.value;
-  
-  todoState.push(taskText);
+  const { target, target: { elements } } = e;
+  const taskText = elements.task.value.trim();
 
-  const listItem = document.createElement('li');
-  listItem.textContent = taskText;
-  todoList.append(listItem);
+  if (taskText) {
+    todoState.push(taskText);
+
+    const listItem = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    deleteBtn.addEventListener('click', deleteHandler);
+
+    listItem.textContent = taskText;
+
+    listItem.append(deleteBtn);
+    todoList.append(listItem);
+  };
+  
+function deleteHandler(e) {
+  e.target.parentElement.remove();
+  
+  const index = todoState.indexOf(e.target.parentElement.firstChild.textContent);
+
+  todoState.splice(index, 1);
+}
+
 
   target.reset();
 };
